cba9482c41cba06fa444fa0f655cca4d6424019c
LiteX Robot <robot@timvideos.us>
2022-06-10 08:14:42 +0000

    Updating pythondata-cpu-cv32e40s to 0.4.0.post154
    Updated data to 0.4.0-12-g150e9241 based on 150e924186a63b3db2f6080870ccf8f867144208 from https://github.com/openhwgroup/cv32e40s.
    > commit 150e924186a63b3db2f6080870ccf8f867144208
    > Merge: b39deb56 3c6b1c26
    > Author: silabs-oysteink <66771756+silabs-oysteink@users.noreply.github.com>
    > Date:   Fri Jun 10 09:34:04 2022 +0200
    >
    >     Merge pull request #227 from silabs-oysteink/silabs-oysteink_csr-rd-error-fix
    >
    >     Tied off CLIC CSR rd_error signals when SMCLIC=0.
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40s/__init__.py
+++ pythondata_cpu_cv32e40s/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40s"
 
 # Module version
-version_str = "0.4.0.post152"
-version_tuple = (0, 4, 0, 152)
+version_str = "0.4.0.post154"
+version_tuple = (0, 4, 0, 154)
 try:
     from packaging.version import Version as V
-    pversion = V("0.4.0.post152")
+    pversion = V("0.4.0.post154")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.4.0.post10"
-data_version_tuple = (0, 4, 0, 10)
+data_version_str = "0.4.0.post12"
+data_version_tuple = (0, 4, 0, 12)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.4.0.post10")
+    pdata_version = V("0.4.0.post12")
 except ImportError:
     pass
-data_git_hash = "b39deb561e8ed5506eaeec6db3d0ad4148c56908"
-data_git_describe = "0.4.0-10-gb39deb56"
+data_git_hash = "150e924186a63b3db2f6080870ccf8f867144208"
+data_git_describe = "0.4.0-12-g150e9241"
 data_git_msg = """\
-commit b39deb561e8ed5506eaeec6db3d0ad4148c56908
-Merge: 17e60d8e 5516ac3e
+commit 150e924186a63b3db2f6080870ccf8f867144208
+Merge: b39deb56 3c6b1c26
 Author: silabs-oysteink <66771756+silabs-oysteink@users.noreply.github.com>
-Date:   Wed Jun 8 16:29:25 2022 +0200
+Date:   Fri Jun 10 09:34:04 2022 +0200
 
-    Merge pull request #226 from Silabs-ArjanB/ArjanB_misax
+    Merge pull request #227 from silabs-oysteink/silabs-oysteink_csr-rd-error-fix
     
-    Fix for issue #70
+    Tied off CLIC CSR rd_error signals when SMCLIC=0.
 
 """
 
