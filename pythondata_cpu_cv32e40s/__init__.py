cc884c8a5d1dbbf027702fabf938b20457fee760
LiteX Robot <robot@timvideos.us>
2022-06-14 09:13:34 +0000

    Updating pythondata-cpu-cv32e40s to 0.4.0.post156
    Updated data to 0.4.0-14-gd12bfca7 based on d12bfca79a20d6b64138fd43344fe9c3797ec02f from https://github.com/openhwgroup/cv32e40s.
    > commit d12bfca79a20d6b64138fd43344fe9c3797ec02f
    > Merge: 150e9241 ae2bceae
    > Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
    > Date:   Tue Jun 14 10:49:33 2022 +0200
    >
    >     Merge pull request #228 from silabs-oysteink/silabs-oysteink_merge-w24-1
    >
    >     Cherry picked commit for addition of Smstateen CSRs from CV32E40X.
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40s/__init__.py
+++ pythondata_cpu_cv32e40s/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40s"
 
 # Module version
-version_str = "0.4.0.post154"
-version_tuple = (0, 4, 0, 154)
+version_str = "0.4.0.post156"
+version_tuple = (0, 4, 0, 156)
 try:
     from packaging.version import Version as V
-    pversion = V("0.4.0.post154")
+    pversion = V("0.4.0.post156")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.4.0.post12"
-data_version_tuple = (0, 4, 0, 12)
+data_version_str = "0.4.0.post14"
+data_version_tuple = (0, 4, 0, 14)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.4.0.post12")
+    pdata_version = V("0.4.0.post14")
 except ImportError:
     pass
-data_git_hash = "150e924186a63b3db2f6080870ccf8f867144208"
-data_git_describe = "0.4.0-12-g150e9241"
+data_git_hash = "d12bfca79a20d6b64138fd43344fe9c3797ec02f"
+data_git_describe = "0.4.0-14-gd12bfca7"
 data_git_msg = """\
-commit 150e924186a63b3db2f6080870ccf8f867144208
-Merge: b39deb56 3c6b1c26
-Author: silabs-oysteink <66771756+silabs-oysteink@users.noreply.github.com>
-Date:   Fri Jun 10 09:34:04 2022 +0200
+commit d12bfca79a20d6b64138fd43344fe9c3797ec02f
+Merge: 150e9241 ae2bceae
+Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
+Date:   Tue Jun 14 10:49:33 2022 +0200
 
-    Merge pull request #227 from silabs-oysteink/silabs-oysteink_csr-rd-error-fix
+    Merge pull request #228 from silabs-oysteink/silabs-oysteink_merge-w24-1
     
-    Tied off CLIC CSR rd_error signals when SMCLIC=0.
+    Cherry picked commit for addition of Smstateen CSRs from CV32E40X.
 
 """
 
