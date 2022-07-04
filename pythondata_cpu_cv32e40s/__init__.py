50af293043598e26d1305c6786383780ed70d98b
LiteX Robot <robot@timvideos.us>
2022-07-04 06:15:33 +0000

    Updating pythondata-cpu-cv32e40s to 0.4.0.post230
    Updated data to 0.4.0-88-g1da7c409 based on 1da7c4097158094915073c7a89be5fb48549f907 from https://github.com/openhwgroup/cv32e40s.
    > commit 1da7c4097158094915073c7a89be5fb48549f907
    > Merge: 59d63257 b8de6df5
    > Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
    > Date:   Mon Jul 4 08:02:29 2022 +0200
    >
    >     Merge pull request #241 from silabs-oysteink/silabs-oysteink_merge-w27
    >
    >     Merge from CV32E40X
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40s/__init__.py
+++ pythondata_cpu_cv32e40s/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40s"
 
 # Module version
-version_str = "0.4.0.post219"
-version_tuple = (0, 4, 0, 219)
+version_str = "0.4.0.post230"
+version_tuple = (0, 4, 0, 230)
 try:
     from packaging.version import Version as V
-    pversion = V("0.4.0.post219")
+    pversion = V("0.4.0.post230")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.4.0.post77"
-data_version_tuple = (0, 4, 0, 77)
+data_version_str = "0.4.0.post88"
+data_version_tuple = (0, 4, 0, 88)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.4.0.post77")
+    pdata_version = V("0.4.0.post88")
 except ImportError:
     pass
-data_git_hash = "59d63257587ca2a19224c081da450c614075c423"
-data_git_describe = "0.4.0-77-g59d63257"
+data_git_hash = "1da7c4097158094915073c7a89be5fb48549f907"
+data_git_describe = "0.4.0-88-g1da7c409"
 data_git_msg = """\
-commit 59d63257587ca2a19224c081da450c614075c423
-Merge: 81f51ffa 67dfb3c0
-Author: silabs-oysteink <66771756+silabs-oysteink@users.noreply.github.com>
-Date:   Mon Jun 27 09:16:30 2022 +0200
+commit 1da7c4097158094915073c7a89be5fb48549f907
+Merge: 59d63257 b8de6df5
+Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
+Date:   Mon Jul 4 08:02:29 2022 +0200
 
-    Merge pull request #240 from Silabs-ArjanB/ArjanV_secsd
+    Merge pull request #241 from silabs-oysteink/silabs-oysteink_merge-w27
     
-    Fix of secureseed*_n for RVFI
+    Merge from CV32E40X
 
 """
 
