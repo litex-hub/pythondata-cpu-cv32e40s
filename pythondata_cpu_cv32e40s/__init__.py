826aa3e2136c6adf7846cdba31985a7d2e0679d6
LiteX Robot <robot@timvideos.us>
2022-07-18 12:19:15 +0000

    Updating pythondata-cpu-cv32e40s to 0.4.0.post272
    Updated data to 0.4.0-130-gfcca1616 based on fcca1616576762304d5e323c49b505c0c284558a from https://github.com/openhwgroup/cv32e40s.
    > commit fcca1616576762304d5e323c49b505c0c284558a
    > Merge: 70793cc8 c3cb5b93
    > Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
    > Date:   Mon Jul 18 13:51:26 2022 +0200
    >
    >     Merge pull request #263 from Silabs-ArjanB/ArjanB_mrv
    >
    >     Added reset value for rvfi_mode
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40s/__init__.py
+++ pythondata_cpu_cv32e40s/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40s"
 
 # Module version
-version_str = "0.4.0.post270"
-version_tuple = (0, 4, 0, 270)
+version_str = "0.4.0.post272"
+version_tuple = (0, 4, 0, 272)
 try:
     from packaging.version import Version as V
-    pversion = V("0.4.0.post270")
+    pversion = V("0.4.0.post272")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.4.0.post128"
-data_version_tuple = (0, 4, 0, 128)
+data_version_str = "0.4.0.post130"
+data_version_tuple = (0, 4, 0, 130)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.4.0.post128")
+    pdata_version = V("0.4.0.post130")
 except ImportError:
     pass
-data_git_hash = "70793cc8e187fcc2c49a752402af82efd1df958d"
-data_git_describe = "0.4.0-128-g70793cc8"
+data_git_hash = "fcca1616576762304d5e323c49b505c0c284558a"
+data_git_describe = "0.4.0-130-gfcca1616"
 data_git_msg = """\
-commit 70793cc8e187fcc2c49a752402af82efd1df958d
-Merge: c70bf4e1 70ceb3aa
+commit fcca1616576762304d5e323c49b505c0c284558a
+Merge: 70793cc8 c3cb5b93
 Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
-Date:   Mon Jul 18 12:56:45 2022 +0200
+Date:   Mon Jul 18 13:51:26 2022 +0200
 
-    Merge pull request #261 from Silabs-ArjanB/ArjanB_249b
+    Merge pull request #263 from Silabs-ArjanB/ArjanB_mrv
     
-    PMP fix (additional fix for issue 249)
+    Added reset value for rvfi_mode
 
 """
 
