1b41662711cea24e5dd220bc2f9fdd6d0ce7a474
LiteX Robot <robot@timvideos.us>
2022-09-26 09:19:38 +0000

    Updating pythondata-cpu-cv32e40s to 0.5.0.post208
    Updated data to 0.5.0-66-g7568910d based on 7568910d50da36509677aad69e8c109aeb45ce73 from https://github.com/openhwgroup/cv32e40s.
    > commit 7568910d50da36509677aad69e8c109aeb45ce73
    > Merge: 2e710384 7e370cc9
    > Author: silabs-oysteink <66771756+silabs-oysteink@users.noreply.github.com>
    > Date:   Mon Sep 26 10:27:59 2022 +0200
    >
    >     Merge pull request #306 from Silabs-ArjanB/ArjanB_cpuctrl1
    >
    >     Turned some security features to default on, changing the reset value…
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40s/__init__.py
+++ pythondata_cpu_cv32e40s/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40s"
 
 # Module version
-version_str = "0.5.0.post206"
-version_tuple = (0, 5, 0, 206)
+version_str = "0.5.0.post208"
+version_tuple = (0, 5, 0, 208)
 try:
     from packaging.version import Version as V
-    pversion = V("0.5.0.post206")
+    pversion = V("0.5.0.post208")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.5.0.post64"
-data_version_tuple = (0, 5, 0, 64)
+data_version_str = "0.5.0.post66"
+data_version_tuple = (0, 5, 0, 66)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.5.0.post64")
+    pdata_version = V("0.5.0.post66")
 except ImportError:
     pass
-data_git_hash = "2e7103847abc86f987dfa45aa5acacf7355ffc01"
-data_git_describe = "0.5.0-64-g2e710384"
+data_git_hash = "7568910d50da36509677aad69e8c109aeb45ce73"
+data_git_describe = "0.5.0-66-g7568910d"
 data_git_msg = """\
-commit 2e7103847abc86f987dfa45aa5acacf7355ffc01
-Merge: d115a782 b95ae092
+commit 7568910d50da36509677aad69e8c109aeb45ce73
+Merge: 2e710384 7e370cc9
 Author: silabs-oysteink <66771756+silabs-oysteink@users.noreply.github.com>
-Date:   Wed Sep 21 14:51:07 2022 +0200
+Date:   Mon Sep 26 10:27:59 2022 +0200
 
-    Merge pull request #304 from silabs-oysteink/silabs-oysteink_rchk_albuf
+    Merge pull request #306 from Silabs-ArjanB/ArjanB_cpuctrl1
     
-    Instruction side integrity check
+    Turned some security features to default on, changing the reset value…
 
 """
 
