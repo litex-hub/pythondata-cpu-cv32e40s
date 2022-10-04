7da8487f17454b6b845479fea46b74621473c701
LiteX Robot <robot@timvideos.us>
2022-10-04 13:51:49 +0000

    Updating pythondata-cpu-cv32e40s to 0.5.0.post268
    Updated data to 0.5.0-126-g4a9ff950 based on 4a9ff950f56136863eaff7a11baa83b73fcafab9 from https://github.com/openhwgroup/cv32e40s.
    > commit 4a9ff950f56136863eaff7a11baa83b73fcafab9
    > Merge: 736e1e42 a91905d2
    > Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
    > Date:   Tue Oct 4 15:25:03 2022 +0200
    >
    >     Merge pull request #314 from silabs-oysteink/protocol_harden
    >
    >     OBI protocol hardening
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40s/__init__.py
+++ pythondata_cpu_cv32e40s/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40s"
 
 # Module version
-version_str = "0.5.0.post264"
-version_tuple = (0, 5, 0, 264)
+version_str = "0.5.0.post268"
+version_tuple = (0, 5, 0, 268)
 try:
     from packaging.version import Version as V
-    pversion = V("0.5.0.post264")
+    pversion = V("0.5.0.post268")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.5.0.post122"
-data_version_tuple = (0, 5, 0, 122)
+data_version_str = "0.5.0.post126"
+data_version_tuple = (0, 5, 0, 126)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.5.0.post122")
+    pdata_version = V("0.5.0.post126")
 except ImportError:
     pass
-data_git_hash = "736e1e4253b50ed550bf6adeebc6fd71cf19dca4"
-data_git_describe = "0.5.0-122-g736e1e42"
+data_git_hash = "4a9ff950f56136863eaff7a11baa83b73fcafab9"
+data_git_describe = "0.5.0-126-g4a9ff950"
 data_git_msg = """\
-commit 736e1e4253b50ed550bf6adeebc6fd71cf19dca4
-Merge: 8816f407 ebec85ec
+commit 4a9ff950f56136863eaff7a11baa83b73fcafab9
+Merge: 736e1e42 a91905d2
 Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
-Date:   Tue Oct 4 10:13:19 2022 +0200
+Date:   Tue Oct 4 15:25:03 2022 +0200
 
-    Merge pull request #316 from silabs-oysteink/silabs-oysteink_merge-w40-1
+    Merge pull request #314 from silabs-oysteink/protocol_harden
     
-    Merge from CV32E40X
+    OBI protocol hardening
 
 """
 
