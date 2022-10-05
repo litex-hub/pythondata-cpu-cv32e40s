5bee222407260337b50589ef1daf7ff38faef3c4
LiteX Robot <robot@timvideos.us>
2022-10-05 12:23:40 +0000

    Updating pythondata-cpu-cv32e40s to 0.5.0.post272
    Updated data to 0.5.0-130-g4f08c049 based on 4f08c0490f93da2f32b7f937b23d349a2aa56edf from https://github.com/openhwgroup/cv32e40s.
    > commit 4f08c0490f93da2f32b7f937b23d349a2aa56edf
    > Merge: 3bf3aa78 ede8ee81
    > Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
    > Date:   Wed Oct 5 13:59:48 2022 +0200
    >
    >     Merge pull request #318 from silabs-oysteink/issue_293
    >
    >     Fix for issue #293.
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40s/__init__.py
+++ pythondata_cpu_cv32e40s/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40s"
 
 # Module version
-version_str = "0.5.0.post268"
-version_tuple = (0, 5, 0, 268)
+version_str = "0.5.0.post272"
+version_tuple = (0, 5, 0, 272)
 try:
     from packaging.version import Version as V
-    pversion = V("0.5.0.post268")
+    pversion = V("0.5.0.post272")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.5.0.post126"
-data_version_tuple = (0, 5, 0, 126)
+data_version_str = "0.5.0.post130"
+data_version_tuple = (0, 5, 0, 130)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.5.0.post126")
+    pdata_version = V("0.5.0.post130")
 except ImportError:
     pass
-data_git_hash = "4a9ff950f56136863eaff7a11baa83b73fcafab9"
-data_git_describe = "0.5.0-126-g4a9ff950"
+data_git_hash = "4f08c0490f93da2f32b7f937b23d349a2aa56edf"
+data_git_describe = "0.5.0-130-g4f08c049"
 data_git_msg = """\
-commit 4a9ff950f56136863eaff7a11baa83b73fcafab9
-Merge: 736e1e42 a91905d2
+commit 4f08c0490f93da2f32b7f937b23d349a2aa56edf
+Merge: 3bf3aa78 ede8ee81
 Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
-Date:   Tue Oct 4 15:25:03 2022 +0200
+Date:   Wed Oct 5 13:59:48 2022 +0200
 
-    Merge pull request #314 from silabs-oysteink/protocol_harden
+    Merge pull request #318 from silabs-oysteink/issue_293
     
-    OBI protocol hardening
+    Fix for issue #293.
 
 """
 
