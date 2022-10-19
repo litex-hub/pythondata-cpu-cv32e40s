5bd7fe7d6be0ad31e0add1be1126184e862fd52c
LiteX Robot <robot@timvideos.us>
2022-10-19 13:52:13 +0000

    Updating pythondata-cpu-cv32e40s to 0.6.0.post157
    Updated data to 0.6.0-15-gd6f68a6a based on d6f68a6a7e2005a4968d1c9ade5d8b2416eafd7d from https://github.com/openhwgroup/cv32e40s.
    > commit d6f68a6a7e2005a4968d1c9ade5d8b2416eafd7d
    > Merge: 020677aa 836dac1b
    > Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
    > Date:   Wed Oct 19 15:46:53 2022 +0200
    >
    >     Merge pull request #329 from silabs-oivind/issue_227_assertion
    >
    >     Add assertion for issue #277. mstatus.mprv is cleared when entering u…
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40s/__init__.py
+++ pythondata_cpu_cv32e40s/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40s"
 
 # Module version
-version_str = "0.6.0.post155"
-version_tuple = (0, 6, 0, 155)
+version_str = "0.6.0.post157"
+version_tuple = (0, 6, 0, 157)
 try:
     from packaging.version import Version as V
-    pversion = V("0.6.0.post155")
+    pversion = V("0.6.0.post157")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.6.0.post13"
-data_version_tuple = (0, 6, 0, 13)
+data_version_str = "0.6.0.post15"
+data_version_tuple = (0, 6, 0, 15)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.6.0.post13")
+    pdata_version = V("0.6.0.post15")
 except ImportError:
     pass
-data_git_hash = "020677aaf6dec3fb04e13cee9dbb0679d8e5ab15"
-data_git_describe = "0.6.0-13-g020677aa"
+data_git_hash = "d6f68a6a7e2005a4968d1c9ade5d8b2416eafd7d"
+data_git_describe = "0.6.0-15-gd6f68a6a"
 data_git_msg = """\
-commit 020677aaf6dec3fb04e13cee9dbb0679d8e5ab15
-Merge: 51abba01 40f30d2f
+commit d6f68a6a7e2005a4968d1c9ade5d8b2416eafd7d
+Merge: 020677aa 836dac1b
 Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
-Date:   Wed Oct 19 08:41:04 2022 +0200
+Date:   Wed Oct 19 15:46:53 2022 +0200
 
-    Merge pull request #328 from silabs-oysteink/silabs-oysteink_merge-w42-1
+    Merge pull request #329 from silabs-oivind/issue_227_assertion
     
-    Merge from CV32E40X.
+    Add assertion for issue #277. mstatus.mprv is cleared when entering u…
 
 """
 
