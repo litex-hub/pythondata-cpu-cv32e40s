dd41ec1cf33aae94591c31adb1cd901039e9ce27
LiteX Robot <robot@timvideos.us>
2022-10-27 08:16:50 +0000

    Updating pythondata-cpu-cv32e40s to 0.6.0.post164
    Updated data to 0.6.0-22-g6ef4354d based on 6ef4354dac60807511e83ec95a8c5ea6e1e5150d from https://github.com/openhwgroup/cv32e40s.
    > commit 6ef4354dac60807511e83ec95a8c5ea6e1e5150d
    > Merge: 2878f7cb 55e5c909
    > Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
    > Date:   Thu Oct 27 09:57:07 2022 +0200
    >
    >     Merge pull request #331 from silabs-oysteink/minhv_fix-1
    >
    >     Updates to mret/xinhv handling.
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40s/__init__.py
+++ pythondata_cpu_cv32e40s/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40s"
 
 # Module version
-version_str = "0.6.0.post161"
-version_tuple = (0, 6, 0, 161)
+version_str = "0.6.0.post164"
+version_tuple = (0, 6, 0, 164)
 try:
     from packaging.version import Version as V
-    pversion = V("0.6.0.post161")
+    pversion = V("0.6.0.post164")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.6.0.post19"
-data_version_tuple = (0, 6, 0, 19)
+data_version_str = "0.6.0.post22"
+data_version_tuple = (0, 6, 0, 22)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.6.0.post19")
+    pdata_version = V("0.6.0.post22")
 except ImportError:
     pass
-data_git_hash = "2878f7cba028fb201f4a84bc1feea5059813d503"
-data_git_describe = "0.6.0-19-g2878f7cb"
+data_git_hash = "6ef4354dac60807511e83ec95a8c5ea6e1e5150d"
+data_git_describe = "0.6.0-22-g6ef4354d"
 data_git_msg = """\
-commit 2878f7cba028fb201f4a84bc1feea5059813d503
-Merge: d6f68a6a 3fd58d5b
+commit 6ef4354dac60807511e83ec95a8c5ea6e1e5150d
+Merge: 2878f7cb 55e5c909
 Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
-Date:   Thu Oct 20 09:59:11 2022 +0200
+Date:   Thu Oct 27 09:57:07 2022 +0200
 
-    Merge pull request #330 from silabs-oivind/merge_cv32e40x
+    Merge pull request #331 from silabs-oysteink/minhv_fix-1
     
-    Merge cv32e40x
+    Updates to mret/xinhv handling.
 
 """
 
